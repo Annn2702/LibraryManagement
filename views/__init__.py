8202e25e8aaccc6038d90419373cb3ed210bbae5
quocan04 <ant962111@gmail.com>
2025-12-13 17:22:29 +0700

    Updata login_Staff

--- views/__init__.py
+++ views/__init__.py
@@ -2,5 +2,7 @@
 from .main_window import MainWindow
 from .reader_view import ReaderView
 from .reader_dialog import ReaderDialog
+from .staff_login_view import StaffLoginView
+from .staff_view import StaffView
 
 __all__ = ['MainWindow', 'ReaderView', 'ReaderDialog', 'borrow_view', 'book_view','book_dialog','report_view', 'system_view', 'StaffView', 'StaffLoginView']
