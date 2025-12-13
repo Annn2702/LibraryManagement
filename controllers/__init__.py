01d8365a297f99bb3f69e4a8c7ab589e2366d0a5
quoclam2111 <quoclam2111@gmail.com>
2025-12-13 13:24:10 +0700

    update giao diện

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,4 +1,7 @@
+
 """Controllers package"""
 from .reader_controller import ReaderController
+from .book_controller import BookController
+from .borrow_controller import BorrowController
 
-__all__ = ['ReaderController']
+__all__ = ['ReaderController', 'BookController', 'BorrowController']
